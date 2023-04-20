ca6a71e4307f729fc61234cdb4730be5c7b02a18
Axel SILVESTRE <a.silvestre@lyon.ort.asso.fr>
2023-04-20 14:04:50 +0200

    Step 2

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.red}>Open up <Text style={styles.bold}>App.js</Text> to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        {sampleGoals.map((goal) =>{
+          return (
+          <View>
+            <Text style={styles.list}>{`\u2022 ${goal.key}`}</Text>
+          </View>
+          );
+        })}
+      </View>  
+      <View style={styles.row}>
+        <TextInput placeholder="Nouvel objectif ?"></TextInput>
+        <Button title="Ajouter" onPress={() => alert('OK !')}/>
+      </View>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -19,8 +32,29 @@ const styles = StyleSheet.create({
   },
   red: {
     color: 'red',
+    marginBottom: 40,
   },
   bold: {
     fontWeight: 'bold',
   },
+  list: {
+    marginBottom: 10,
+  },
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
 });
+
+const sampleGoals =[
+  {key: "Faire les courses"},
+  {key: "Aller à la salle 3 fois par semaine"},
+  {key: "Monter à plus de 5000m d'altitude"},
+  {key: "Acheter mon premier appartement"},
+  {key: "Perdre 5kg"},
+  {key: "Gagner en productivite"},
+  {key: "Apprendre un nouveau langage"},
+  {key: "Faire une mission en freelance"},
+  {key: "Organiser un meetup autour de la tech"},
+  {key: "Faire un triathlon"},
+];
